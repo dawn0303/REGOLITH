48b808427b05c416816b2ef85122af2748f0bca1
dawn0303 <60470011+dawn0303@users.noreply.github.com>
2024-02-28 13:41:01 +0000

    added the scene for deployable cover

--- shield.gd
+++ shield.gd
@@ -1,9 +1,13 @@
 extends StaticBody3D
 
 var equipped = false
+var team
+var player
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+		if player == null:
+			player = $"../../../.."
+		team = player.team
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
